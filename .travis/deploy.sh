ac23e49f0a602c7701399594c63a86ca94712943
touhonoob <touhonoob@gmail.com>
2015-11-01 00:26:11 +0800

    remove .gitignore under dist

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd dist
+find . -type f -name .gitignore -exec rm {} \;
 git init
 git commit -m "Deploy to GitHub Pages $(date +"%Y-%m-%d %T")"
 git push --force git@github.com:PSEJP/pse.or.jp.git master:gh-pages
